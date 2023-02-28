9fa59c879f19883d82b875fe86d7b5cdba65d9f6
Axl-web <axl-web.dev@protonmail.com>
2023-02-27 22:56:08 -0300

    Mejora del codigo

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,14 @@
-cont = 0
+def fizzbuzz():
+    for i in range(100):
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            print(f'FizzBuzz')
+        elif i % 3 == 0:
+            print(f'Fizz')
+        elif i % 5 == 0:
+            print(f'Buzz')
+        else:
+            print(f'{i}')
 
-while cont < 100:
-    cont += 1
-    if cont % 3 == 0 and cont % 5 == 0:
-        print(f'FizzBuzz')
-    elif cont % 3 == 0:
-        print(f'Fizz')
-    elif cont % 5 == 0:
-        print(f'Buzz')
-    else:
-        print(f'{cont}')
+
+fizzbuzz()
