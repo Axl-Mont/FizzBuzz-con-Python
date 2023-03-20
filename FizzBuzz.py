5faa407ed64d405a08265026bfa7ca351ec27c53
Axl-Mont <montenegroaxel@outlook.com>
2023-03-20 20:27:07 -0300

    Se agrego opcion para que el usuario ingrese los rangos donde quiera que se ejecute FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,5 @@
-def fizzbuzz():
-    for i in range(100):
-        i += 1
+def fizzbuzz(rangoA,rangoB):
+    for i in range(rangoA, rangoB + 1):
         if i % 3 == 0 and i % 5 == 0:
             print(f'FizzBuzz')
         elif i % 3 == 0:
@@ -10,5 +9,11 @@ def fizzbuzz():
         else:
             print(f'{i}')
 
+def inicio ():
+    print(' Bienvenido a Fizz Buzz FizzBuzz')
+    inicio = int(input('Ingresa el número donde quieres Ininiciar : '))
+    fin = int(input('Ingresea el número donde quieras Finalizar: '))
+    fizzbuzz(inicio,fin)
 
-fizzbuzz()
+inicio()
+input()
